38aee9158db614a096cac7319006f4edbfc1884e
anasgh2025 <anasghazawi2007@hotmail.com>
2026-03-13 20:39:37 +0400

    fix

--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -1,11 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+const listsController = require('../controllers/listsController');
+
 // Generate invite link for a list
 router.post('/:id/invite', listsController.generateInviteLink);
 
 // Accept invite and link user to list
 router.post('/accept-invite', listsController.acceptInvite);
-const express = require('express');
-const router = express.Router();
-const listsController = require('../controllers/listsController');
 
 // GET all lists
 router.get('/', listsController.getAllLists);
